0d92ead8384d0e7bd13ce16953482578c6e8e91b
hyebin <sksp4334@naver.com>
2022-06-20 20:07:02 +0900

    remove: Layout에 사용하지 않는 코드 삭제

--- src/components/layout/app/Layout.tsx
+++ src/components/layout/app/Layout.tsx
@@ -1,16 +1,13 @@
-import React, { ReactElement, useState } from "react"
-import LoginModal from "../../login/LoginModal"
+import React, { ReactElement } from "react"
 import Footer from "../../common/Footer"
 import Header from "../../common/Header"
 
 export default function Layout({ children }: { children: ReactElement }) {
-  const [isModal, setIsModal] = useState(false)
   return (
     <>
       <Header />
       {children}
       <Footer />
-      {isModal && <LoginModal />}
     </>
   )
 }
